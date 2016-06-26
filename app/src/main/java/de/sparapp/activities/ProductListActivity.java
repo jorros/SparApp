c836916eec1fdd42e311b90fe6265bb349131043
jorros <joergrosenauer@gmail.com>
2016-06-26 13:57:21 +0100

    Refactoring...

--- app/src/main/java/de/sparapp/activities/ProductListActivity.java
+++ app/src/main/java/de/sparapp/activities/ProductListActivity.java
@@ -1,24 +1,16 @@
 package de.sparapp.activities;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 
-import de.sparapp.fragments.ProductDetailFragment;
 import de.sparapp.R;
-import de.sparapp.entities.Product;
+import de.sparapp.adapters.ProductsRecyclerViewAdapter;
 import de.sparapp.providers.ProductProvider;
-
-import java.util.List;
 
 /**
  * An activity representing a list of Products. This activity
@@ -29,14 +21,7 @@ import java.util.List;
  * item details side-by-side using two vertical panes.
  */
 public class ProductListActivity extends AppCompatActivity {
-
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
-    private boolean mTwoPane;
-
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
@@ -49,85 +34,9 @@ public class ProductListActivity extends AppCompatActivity {
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
 
-        if (findViewById(R.id.product_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
-        }
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(ProductProvider.getProducts()));
-    }
-
-    public class SimpleItemRecyclerViewAdapter
-            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
-
-        private final List<Product> mValues;
-
-        public SimpleItemRecyclerViewAdapter(List<Product> items) {
-            mValues = items;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.product_list_content, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
-            holder.mProduct = mValues.get(position);
-            holder.mIdView.setText(Integer.toString(mValues.get(position).getId()));
-            holder.mContentView.setText(mValues.get(position).getTitle());
-
-            holder.mView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (mTwoPane) {
-                        Bundle arguments = new Bundle();
-                        arguments.putInt(ProductDetailFragment.ARG_ITEM_ID, holder.mProduct.getId());
-                        ProductDetailFragment fragment = new ProductDetailFragment();
-                        fragment.setArguments(arguments);
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.product_detail_container, fragment)
-                                .commit();
-                    } else {
-                        Context context = v.getContext();
-                        Intent intent = new Intent(context, ProductDetailActivity.class);
-                        intent.putExtra(ProductDetailFragment.ARG_ITEM_ID, holder.mProduct.getId());
-
-                        context.startActivity(intent);
-                    }
-                }
-            });
-        }
-
-        @Override
-        public int getItemCount() {
-            return mValues.size();
-        }
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            public final View mView;
-            public final TextView mIdView;
-            public final TextView mContentView;
-            public Product mProduct;
-
-            public ViewHolder(View view) {
-                super(view);
-                mView = view;
-                mIdView = (TextView) view.findViewById(R.id.id);
-                mContentView = (TextView) view.findViewById(R.id.content);
-            }
-
-            @Override
-            public String toString() {
-                return super.toString() + " '" + mContentView.getText() + "'";
-            }
-        }
+        recyclerView.setAdapter(new ProductsRecyclerViewAdapter(ProductProvider.getProducts()));
     }
 }
